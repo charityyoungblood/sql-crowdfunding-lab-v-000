9b654ef394f50e12fa3781af7c9e5ea111e6984c
Charity <Youngblood.charity@gmail.com>
2018-01-11 15:34:42 -0800

    added JOIN keyword and foreign key to search query in sql _queries.rb

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 # Write your sql queries in this file in the appropriate method like the example below:
 #
 # def select_category_from_projects
@@ -6,15 +8,15 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
+def       selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   # the SUM of pledge amounts based on project_id
   # select the amounts where projects.id = pledges.project_id
-  "SELECT SUM(amount) FROM pledges WHERE project_id = 3"
-#"SELECT TOTAL(amount) FROM projects, pledges WHERE projects.id = 1"
+"SELECT projects.title, SUM(pledges.amount) FROM pledges JOIN projects ON projects.id = pledges.project_id GROUP BY projects.title ORDER BY title ASC"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+  # break down query little by little until you can create query for the whole thing
+  "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id ORDER BY name"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
