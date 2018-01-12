097235fda23b9b8db9411ec4559f7c3a39c725a4
Charity <Youngblood.charity@gmail.com>
2018-01-11 18:22:35 -0800

    added amount of goal query to sql_queries.rb

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   # # break down query little by little until you can create query for the whole thing
-"Write your SQL query Here"
+  "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS remaining FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING remaining >= 0"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
