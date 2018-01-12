7b9ad71af2d28e186a2447bacf61476336894c0a
Charity <Youngblood.charity@gmail.com>
2018-01-12 12:49:12 -0800

    added JOIN to query in category method in sql_queries.rb

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   # break down query little by little until you can create query for the whole thing
-"SELECT projects.cateogory FROM projects"
+"SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
